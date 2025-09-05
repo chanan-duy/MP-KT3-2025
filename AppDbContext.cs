2769d009e480992a3e98078321c63ed9864c0f1a
chanan-duy <chanan.duy@proton.me>
2025-09-05 10:04:12 +0500

    Init AppDbContext

---
+++ AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MP_KT3_2025;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=database1.db");
+    }
+}
