e1d369cff93f21384e3fa1171b07d4d5a0357a66
chanan-duy <chanan.duy@proton.me>
2025-09-05 09:09:30 +0500

    User init

---
+++ User.cs
@@ -0,0 +1,19 @@
+﻿namespace MP_KT3_2025;
+
+public class User
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string DateOfBirth { get; set; }
+
+    public User(int id, string firstName, string lastName, string email, string dateOfBirth)
+    {
+        Id = id;
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+        DateOfBirth = dateOfBirth;
+    }
+}
