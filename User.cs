65dd124e3ecdca036fdc9ba511423cc3e691273d
chanan-duy <chanan.duy@proton.me>
2025-09-05 10:28:03 +0500

    User add date of birth validator

--- User.cs
+++ User.cs
@@ -1,12 +1,29 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
 
 namespace MP_KT3_2025;
 
+[Index(nameof(Email), IsUnique = true)]
 public class User
 {
     [Key] public Guid Id { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string Email { get; set; }
-    public DateOnly DateOfBirth { get; set; }
+    [Required] public string FirstName { get; set; }
+    [Required] public string LastName { get; set; }
+    [Required] public string Email { get; set; }
+    [Required] public DateOnly DateOfBirth { get; set; }
+
+    public bool IsValidDateOfBirth()
+    {
+        const int allowedAge = 14;
+
+        var today = DateOnly.FromDateTime(DateTime.Today);
+        var age = today.Year - DateOfBirth.Year;
+
+        if (DateOfBirth > today.AddYears(-age))
+        {
+            age--;
+        }
+
+        return age >= allowedAge;
+    }
 }
